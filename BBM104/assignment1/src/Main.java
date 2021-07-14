2dc1466d22969e3f24a4ea4b3e8b1880ba4a58c0
buseorak <73755546+buseorak@users.noreply.github.com>
2021-07-14 14:26:36 +0300

    Update Main.java

--- BBM104/assignment1/src/Main.java
+++ BBM104/assignment1/src/Main.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        AuthorFileOperations.readAuthorFile(args[0]);
+        // Creating the output file
         MyFileIOOperations.createOutputFile("output.txt");
+        
+        // Reading the input files taken as command-line arguments
+        AuthorFileOperations.readAuthorFile(args[0]);
         ArrayList<String> commands = MyFileIOOperations.readFile(args[1]);
 
+        // Executing the commands
         assert commands != null;
         CommandOperations.executeCommand(commands);
     }
