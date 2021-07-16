d496acb9aca06b5a34caa8eac1b2268a2d237c89
buseorak <73755546+buseorak@users.noreply.github.com>
2021-07-16 15:18:10 +0300

    Update Main.java

--- BBM104/assignment3/src/Main.java
+++ BBM104/assignment3/src/Main.java
@@ -1,1 +1,11 @@
+public class Main {
 
+    public static void main(String[] args) {
+
+        // Read initials file and creating a GameBoard object
+        GameBoard board = InitialsFileReader.readInitialsFile(args[0]);
+
+        // Read the commands file
+        CommandsFileReader.readCommandsFile(args[1], board, args[2]);
+    }
+}
